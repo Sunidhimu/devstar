6a74c0f0452e706276fdbe0ea22e9baef9a0e988
Umed24 <109196694+Umed24@users.noreply.github.com>
2024-06-28 00:21:54 +0530

    creation of flashcard outlining

--- src/routes/(tools)/online-flashcard/+page.svelte
+++ src/routes/(tools)/online-flashcard/+page.svelte
@@ -3,7 +3,15 @@
 </script>
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
-	<!-- Add tool here -->
+	
+</div>
+
+<div class="max-w-md mx-auto p-4">
+	<h1 class="text-xl font-bold mb-4"> Add Flashcard </h1>
+	<input type="text" class="" placeholder="title">
+	<textarea placeholder="description"></textarea>
+	<input type="file" accept="image/*">
+	<button type="submit" class="bg-gray-900 text-white px-4 py-2 rounded hover:bg-blue-600">Save</button>
 </div>
 
 <style>
---
+++ src/routes/(tools)/online-flashcard/routes/Addpage.svelte
@@ -0,0 +1,6 @@
+<div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
+    <p>Page added </p>
+</div>
+
+
+
