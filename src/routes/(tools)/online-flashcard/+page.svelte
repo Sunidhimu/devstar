0f1004269802a9bf7773a6cf3d0abc7e53d0e786
Kavyanjali02 <129305336+Kavyanjali02@users.noreply.github.com>
2024-07-12 22:42:34 +0530

    Update +page.svelte

--- src/routes/(tools)/online-flashcard/+page.svelte
+++ src/routes/(tools)/online-flashcard/+page.svelte
@@ -116,7 +116,7 @@
 	  <button type="submit" class="bg-gray-900 text-white px-4 py-2 rounded hover:bg-blue-600" on:click={saveFlashcard}>Save</button>
 	</div>
   {:else}
-	<div class="style-container">
+	<div class="slide-container">
 	  <h1 class="text-xl font-bold mb-4">Flashcards</h1>
 	  {#if $flashcards.length > 0}
 		<div class="p-4 mb-4 border border-gray-400 rounded shadow">
